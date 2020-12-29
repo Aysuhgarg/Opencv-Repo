5b125991d9505a468126b114450dc33556ebbfab
Aysuhgarg <Coolayushgarg222@gmail.com>
2020-12-29 23:12:38 +0530

    Updation

--- function.py
+++ function.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 img=cv2.imread("anon-hack.png")
                                              #1 BGR to Gray
 img2=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -7,4 +8,16 @@ cv2.waitKey(0)
                                             #2 GaussianBlur
 imgblur=cv2.GaussianBlur(img2,(7,7),0)
 cv2.imshow("Gaussian image",imgblur)
+
+imgcanny=cv2.Canny(img2,100,100)
+cv2.imshow("Canny image",imgcanny)
+
+kernel=np.ones((5,5),np.unit8)
+imgDialation=cv2.dilate(imgcanny,Kernel,iterations=100)
+#where kernel=np.ones((5,5),np.unit 8)
+cv2.imshow("Canny image",imgcanny)
+
+imgEroded=cv2.crode(imgDialation,kernel,iterations=100)
+cv2.imshow("Canny image",imgEroded)
+
 cv2.waitKey(0)
